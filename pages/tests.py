002ff87dfc83e679346672b2cdfb046fa39c6907
Nii-Armah <stanleyniiarmah7@gmail.com>
2023-05-18 15:51:26 +0000

    Create a minimal homepage for the website
    Wire the homepage to the website domain's root url.

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+"""Tests for the functionality of the pages application"""
 
-# Create your tests here.
+from django.test import SimpleTestCase
+
+
+class HomePageTest(SimpleTestCase):
+    """Tests for the homepage of the website"""
+    def test_domain_root_is_a_valid_url(self):
+        """The website's domain root is a valid application url."""
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_domain_root_returns_homepage_of_website(self):
+        """The website's domain root returns the homepage of the website."""
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'home.html')
+
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""The views of the `pages` application."""
 
-# Create your views here.
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.views import View
+
+
+class HomePageView(View):
+    """The logic for the functionality of the website's homepage."""
+    template_name = 'home.html'
+
+    def get(self, request: HttpRequest):
+        """Return the Homepage of the website."""
+        return render(request, self.template_name)
