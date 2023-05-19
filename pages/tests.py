8bca5446682b1a24d2828d00e5ca1b6956543ffe
Nii-Armah <stanleyniiarmah7@gmail.com>
2023-05-19 05:16:08 +0000

    Update internal documentation of tests.py and views.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,17 +1,22 @@
-"""Tests for the functionality of the pages application"""
+"""
+Tests for the functionality of the pages application
+
+TestCases:
+    HomePageTest:
+        Tests for the functionality of the website's homepage.
+"""
 
 from django.test import SimpleTestCase
 
 
 class HomePageTest(SimpleTestCase):
-    """Tests for the homepage of the website"""
+    """Tests for the homepage of the website."""
     def test_domain_root_is_a_valid_url(self):
-        """The website's domain root is a valid application url."""
+        """The website domain's root is a valid application url."""
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
 
-    def test_domain_root_returns_homepage_of_website(self):
-        """The website's domain root returns the homepage of the website."""
+    def test_domain_root_url_returns_homepage_of_website(self):
+        """The website domain's root url returns the homepage of the website."""
         response = self.client.get('')
         self.assertTemplateUsed(response, 'home.html')
-
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
-"""The views of the `pages` application."""
+"""
+The views of the `pages` application.
+
+Views:
+    HomePageView:
+        The logic for the backend functionality of the website's homepage.
+"""
 
 from django.http import HttpRequest
 from django.shortcuts import render
@@ -6,9 +12,9 @@ from django.views import View
 
 
 class HomePageView(View):
-    """The logic for the functionality of the website's homepage."""
+    """The logic for the backend functionality of the website's homepage."""
     template_name = 'home.html'
 
     def get(self, request: HttpRequest):
-        """Return the Homepage of the website."""
+        """Return the homepage of the website."""
         return render(request, self.template_name)
