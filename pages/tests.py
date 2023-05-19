2fc1ecd72fe502ad165cd88f2f0ae6ee7c1dde57
Nii-Armah <stanleyniiarmah7@gmail.com>
2023-05-19 05:25:52 +0000

    Refactor other tests to improve readability

--- pages/tests.py
+++ pages/tests.py
@@ -14,12 +14,12 @@ class HomePageTest(SimpleTestCase):
     """Tests for the homepage of the website."""
     def test_domain_root_is_a_valid_url(self):
         """The website domain's root is a valid application url."""
-        response = self.client.get('')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_domain_root_url_returns_homepage_of_website(self):
         """The website domain's root url returns the homepage of the website."""
-        response = self.client.get('')
+        response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_homepage_url_is_associated_with_the_appropriate_url_name(self):
